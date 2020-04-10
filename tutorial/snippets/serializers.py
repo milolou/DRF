2efff93cfbef8aba46115255044ff253b1e0c96e
milolou <milofromlou@gmail.com>
2020-04-10 11:40:17 +0800

    drf

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight',format='html')
+
+    def create(self,validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    def updated(self,instance,validated_data):
+        instance.title = validated_data.get('title',instance.title)
+        instance.code = validated_data.get('code',instance.code)
+        instance.linenos = validated_data.get('linenos',instance.linenos)
+        instance.language = validated_data.get('language',instance.language)
+        instance.style = validated_data.get('style',instance.style)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = Snippet
+        fields = ('url','id', 'highlight','owner','title','code','linenos','language','style')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True,view_name='snippet-detail',read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url','id','username','snippets']
